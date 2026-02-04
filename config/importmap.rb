f3e731ea6509ff23b56f43b3bbff227d81effd51
sekito <sekito1107gg@gmail.com>
2026-02-05 02:10:51 +0900

    Config: Ace Editorの設定を削除

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,8 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-# Ace Editor (via jsdelivr)
-pin "ace-builds", to: "https://cdn.jsdelivr.net/npm/ace-builds@1.32.6/src-min-noconflict/ace.js"
-pin "ace-builds/mode/ruby", to: "https://cdn.jsdelivr.net/npm/ace-builds@1.32.6/src-min-noconflict/mode-ruby.js"
-pin "ace-builds/theme/chrome", to: "https://cdn.jsdelivr.net/npm/ace-builds@1.32.6/src-min-noconflict/theme-chrome.js"
-pin "ace-builds/theme/one_dark", to: "https://cdn.jsdelivr.net/npm/ace-builds@1.32.6/src-min-noconflict/theme-one_dark.js"
+
